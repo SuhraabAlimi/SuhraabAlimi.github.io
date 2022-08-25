75254122b3cfeed28f9da9ffe9d17bbfaebdab72
Suhraab Alimi <suhraab.alimi@student.allenisd.org>
2022-08-25 14:07:13 -0500

    forst commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function sussy ()
+{
+    location.replace("https://www.google.com/maps/place/1509+riverdale+dr+allen+tx/@33.1230448,-96.7090652,3a,75y,161.51h,90t/data=!3m4!1e1!3m2!1sC1Bv8HfpjfCpfEzisIgkzw!2e0!4m2!3m1!1s0x864c17aad6c4c905:0x605007774ecffb6d?sa=X&ved=2ahUKEwjvrbqu0c75AhVAlWoFHTDmDFQQxB16BAglEAI");
+}
+function isp_sussy ()
+{
+    location.replace("https://www.codermerlin.com/users/suhraab-alimi/Digital%20Portfolio/isp.html");
+}
+function white ()
+{
+    location.replace('https://www.codermerlin.com/users/suhraab-alimi/Digital%20Portfolio/index.html');
+}
+function suessy ()
+{
+    location.replace('https://www.codermerlin.com/users/suhraab-alimi/Digital%20Portfolio/classes.html');
+}
+function important ()
+{
+    location.replace('https://codermerlin.com/users/suhraab-alimi/Digital%20Portfolio/important.html')
+}
+
+
+
+
+

004dd724dd5f987753ba3063b29ba6e7b2dbed36
Suhraab Alimi <suhraab.alimi@student.allenisd.org>
2022-08-29 14:24:39 -0500

    Moved Files

---
+++ CS-III/Essays/index.js
@@ -0,0 +1,30 @@
+
+function sussy ()
+{
+    location.replace("https://www.google.com/maps/place/1509+riverdale+dr+allen+tx/@33.1230448,-96.7090652,3a,75y,161.51h,90t/data=!3m4!1e1!3m2!1sC1Bv8HfpjfCpfEzisIgkzw!2e0!4m2!3m1!1s0x864c17aad6c4c905:0x605007774ecffb6d?sa=X&ved=2ahUKEwjvrbqu0c75AhVAlWoFHTDmDFQQxB16BAglEAI");
+}
+function isp_sussy ()
+{
+    location.replace("isp.html");
+}
+function white ()
+{
+    location.replace('../index.html');
+}
+function suessy ()
+{
+    location.replace('classes.html');
+}
+function important ()
+{
+    location.replace('important.html')
+}
+function fingerk ()
+{
+    location.replace('finger.html')
+}
+
+
+
+
+

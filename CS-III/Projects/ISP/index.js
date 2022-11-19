826f08d9b5b4a2d47642ce5e1321681027e5e7f4
Suhraab Alimi <suhraab.alimi@student.allenisd.org>
2022-11-18 21:57:40 -0600

    Added ISP start

---
+++ CS-III/Projects/ISP/index.js
@@ -0,0 +1,45 @@
+
+function sussy ()
+{
+    location.replace("https://www.google.com/maps/place/1509+riverdale+dr+allen+tx/@33.1230448,-96.7090652,3a,75y,161.51h,90t/data=!3m4!1e1!3m2!1sC1Bv8HfpjfCpfEzisIgkzw!2e0!4m2!3m1!1s0x864c17aad6c4c905:0x605007774ecffb6d?sa=X&ved=2ahUKEwjvrbqu0c75AhVAlWoFHTDmDFQQxB16BAglEAI");
+}
+function isp_sussy ()
+{
+    location.replace("../isp.html");
+}
+function white ()
+{
+    location.replace('../../../index.html');
+}
+function suessy ()
+{
+    location.replace('../classes.html');
+}
+function important ()
+{
+    location.replace('../important.html')
+}
+function fingerk ()
+{
+    location.replace('../finger.html')
+}
+function Gitsussy ()
+{
+    location.replace("https://github.com/neeraj-gogate/Optimal-Sorting")
+}
+function spreadsheetssy()
+{
+    location.replace("https://docs.google.com/spreadsheets/d/1unyC68g51rsel25M3M_R3hD50W83XrZAkFtVoDcQIvI/edit#gid=458637415")
+}
+function spreadsheetssy2()
+{
+    location.replace("https://docs.google.com/spreadsheets/d/1Fmqg2T2eHS22srzYoJpiXKoRcX-QhiJmF4pY88wWH9A/edit#gid=682405177")
+}
+function gregor()
+{
+    location.replace("https://www.codermerlin.com/users/stefano-giron/Digital%20Portfolio/CS-III/Projects/ISP/index.html")
+}
+function samsa()
+{
+    location.replace("https://www.codermerlin.com/users/kevin-sun/Digital%20Portfolio/CS-III/Projects/ISP/index.html")
+}

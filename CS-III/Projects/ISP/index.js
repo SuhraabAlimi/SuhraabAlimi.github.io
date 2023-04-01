b98c660f55c09852747c338469f44d61e6a7d834
Suhraab Alimi <suhraab.alimi@student.allenisd.org>
2023-04-01 01:50:13 -0500

    sig achievements

--- CS-III/Projects/ISP/index.js
+++ CS-III/Projects/ISP/index.js
@@ -47,3 +47,24 @@ function github()
 {
     location.replace("https://www.github.com/CSAllenISD/2023-ISP-TowerDefense")
 }
+function sig12()
+{
+    location.replace("https://github.com/CSAllenISD/2023-ISP-TowerDefense/commit/d549c9ac7a799ddc5980dcfd40ff9d81de801981")
+}
+function sig3()
+{
+    location.replace("https://github.com/CSAllenISD/2023-ISP-TowerDefense/commit/b3d92b341ad8d941bd3181ae570d71f4c95cc8a5")
+}
+function sig4()
+{
+    location.replace("https://github.com/CSAllenISD/2023-ISP-TowerDefense/commit/63a338e9b44888aee7e18886a220d0f0a01bddb3")
+}
+function sig567()
+{
+    location.replace("https://github.com/CSAllenISD/2023-ISP-TowerDefense/commit/83a5ca1734e4172662de0c39f005ca1c93feea68")
+}
+
+function sig89()
+{
+    location.replace("https://github.com/CSAllenISD/2023-ISP-TowerDefense/commit/4bacc0e658bc483f3999dcd6a6856c195efb3885")
+}
